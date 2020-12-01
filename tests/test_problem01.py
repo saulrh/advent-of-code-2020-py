601fffb816f88b2ffc77f126f09496a4e7f9cf7a
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2020-12-01 00:24:10 -0800

    clean up tests a tiny bit

--- tests/test_problem01.py
+++ tests/test_problem01.py
@@ -2,17 +2,16 @@ import unittest
 
 from advent_of_code_2020_py import problem01
 
+EXAMPLE_DATA = [1721, 979, 366, 299, 675, 1456]
 
 class Test01(unittest.TestCase):
     def test_example(self):
-        data = [1721, 979, 366, 299, 675, 1456]
-        pair = problem01.get_point(data, 2020, 2)
+        pair = problem01.get_point(EXAMPLE_DATA, 2020, 2)
         self.assertCountEqual(pair, [1721, 299])
         self.assertEqual(problem01.get_result(pair), 514579)
 
     def test_example2(self):
-        data = [1721, 979, 366, 299, 675, 1456]
-        pair = problem01.get_point(data, 2020, 3)
+        pair = problem01.get_point(EXAMPLE_DATA, 2020, 3)
         self.assertCountEqual(pair, [979, 366, 675])
         self.assertEqual(problem01.get_result(pair), 241861950)
 
