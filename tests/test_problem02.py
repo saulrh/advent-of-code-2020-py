d4837ad7f60f8f056dc1595cb8e99c3ad9a5bce0
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2020-12-01 23:02:44 -0800

    compress problem02 tests

--- tests/test_problem02.py
+++ tests/test_problem02.py
@@ -12,30 +12,34 @@ EXAMPLE_DATA = [
 class Test02(unittest.TestCase):
     def test_parsing(self):
         data = [problem02.Entry.FromLine(e) for e in EXAMPLE_DATA]
-        self.assertEqual(data[0].n1, 1)
-        self.assertEqual(data[0].n2, 3)
-        self.assertEqual(data[0].letter, "a")
-        self.assertEqual(data[0].password, "abcde")
-        self.assertEqual(data[1].n1, 1)
-        self.assertEqual(data[1].n2, 3)
-        self.assertEqual(data[1].letter, "b")
-        self.assertEqual(data[1].password, "cdefg")
-        self.assertEqual(data[2].n1, 2)
-        self.assertEqual(data[2].n2, 9)
-        self.assertEqual(data[2].letter, "c")
-        self.assertEqual(data[2].password, "ccccccccc")
+        self.assertEqual(data[0], problem02.Entry(
+            n1=1,
+            n2=3,
+            letter="a",
+            password="abcde",
+        ))
+        self.assertEqual(data[1], problem02.Entry(
+            n1=1,
+            n2=3,
+            letter="b",
+            password="cdefg",
+        ))
+        self.assertEqual(data[2], problem02.Entry(
+            n1=2,
+            n2=9,
+            letter="c",
+            password="ccccccccc",
+        ))
 
     def test_example_part1(self):
         data = [problem02.Entry.FromLine(e) for e in EXAMPLE_DATA]
-        self.assertTrue(data[0].Part1Valid())
-        self.assertFalse(data[1].Part1Valid())
-        self.assertTrue(data[2].Part1Valid())
+        validation = [d.Part1Valid() for d in data]
+        self.assertEqual(validation, [True, False, True])
 
     def test_example_part2(self):
         data = [problem02.Entry.FromLine(e) for e in EXAMPLE_DATA]
-        self.assertTrue(data[0].Part2Valid())
-        self.assertFalse(data[1].Part2Valid())
-        self.assertFalse(data[2].Part2Valid())
+        validation = [d.Part2Valid() for d in data]
+        self.assertEqual(validation, [True, False, False])
 
 
 if __name__ == "__main__":
