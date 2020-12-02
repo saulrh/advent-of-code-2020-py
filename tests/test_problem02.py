1c0d86df155d1ea5d5837db0c1b1c6e1562a7f67
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2020-12-01 23:04:57 -0800

    simplify problem02 tests even more

--- tests/test_problem02.py
+++ tests/test_problem02.py
@@ -12,34 +12,41 @@ EXAMPLE_DATA = [
 class Test02(unittest.TestCase):
     def test_parsing(self):
         data = [problem02.Entry.FromLine(e) for e in EXAMPLE_DATA]
-        self.assertEqual(data[0], problem02.Entry(
-            n1=1,
-            n2=3,
-            letter="a",
-            password="abcde",
-        ))
-        self.assertEqual(data[1], problem02.Entry(
-            n1=1,
-            n2=3,
-            letter="b",
-            password="cdefg",
-        ))
-        self.assertEqual(data[2], problem02.Entry(
-            n1=2,
-            n2=9,
-            letter="c",
-            password="ccccccccc",
-        ))
+        self.assertEqual(
+            data,
+            [
+                problem02.Entry(
+                    n1=1,
+                    n2=3,
+                    letter="a",
+                    password="abcde",
+                ),
+                problem02.Entry(
+                    n1=1,
+                    n2=3,
+                    letter="b",
+                    password="cdefg",
+                ),
+                problem02.Entry(
+                    n1=2,
+                    n2=9,
+                    letter="c",
+                    password="ccccccccc",
+                ),
+            ],
+        )
 
     def test_example_part1(self):
-        data = [problem02.Entry.FromLine(e) for e in EXAMPLE_DATA]
-        validation = [d.Part1Valid() for d in data]
-        self.assertEqual(validation, [True, False, True])
+        self.assertEqual(
+            [problem02.Entry.FromLine(e).Part1Valid() for e in EXAMPLE_DATA],
+            [True, False, True],
+        )
 
     def test_example_part2(self):
-        data = [problem02.Entry.FromLine(e) for e in EXAMPLE_DATA]
-        validation = [d.Part2Valid() for d in data]
-        self.assertEqual(validation, [True, False, False])
+        self.assertEqual(
+            [problem02.Entry.FromLine(e).Part2Valid() for e in EXAMPLE_DATA],
+            [True, False, False],
+        )
 
 
 if __name__ == "__main__":
