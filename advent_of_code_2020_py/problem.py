cc03ccfd1b819d6ae67dded8579c1ea5a1788886
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2020-12-02 14:16:18 -0800

    Add "get the whole file" function to problem module

--- advent_of_code_2020_py/problem.py
+++ advent_of_code_2020_py/problem.py
@@ -1,12 +1,24 @@
 from typing import Callable, Iterator, TypeVar
 
 ProblemRowType = TypeVar("ProblemRowType")
+ProblemType = TypeVar("ProblemType")
+
+
+def _Filename(problem_number: int) -> str:
+    return f"inputs/problem{problem_number:02}.txt"
+
+
+def GetRaw(
+    problem_number: int, transform: Callable[[str], ProblemType] = lambda x: x
+) -> ProblemType:
+    with open(_Filename(problem_number), "r") as f:
+        return transform(f.read())
 
 
 def Get(
     problem_number: int,
     transform: Callable[[str], ProblemRowType] = lambda x: x,
 ) -> Iterator[ProblemRowType]:
-    with open(f"inputs/problem{problem_number:02}.txt", "r") as f:
+    with open(_Filename(problem_number), "r") as f:
         for line in f:
             yield transform(line)
