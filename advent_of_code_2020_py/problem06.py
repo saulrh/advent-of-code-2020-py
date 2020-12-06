a16aaf620d7afd04ecc08bc549e74cced8163fdc
Saul Reynolds-Haertle <saul@cardea-linux.lan>
2020-12-05 23:02:03 -0800

    simplify day 6 a bit - reduce's last arg is optional

--- advent_of_code_2020_py/problem06.py
+++ advent_of_code_2020_py/problem06.py
@@ -7,33 +7,21 @@ from typing import Iterable, Set
 
 from advent_of_code_2020_py import problem
 
-LETTERS = {chr(i) for i in range(ord("a"), ord("z") + 1)}
-
 
 def Part1Batch(group: Iterable[Set[str]]) -> Set[str]:
-    return functools.reduce(set.union, group, set())
+    return functools.reduce(set.union, group)
 
 
 def Part2Batch(group: Iterable[Set[str]]) -> Set[str]:
-    return functools.reduce(set.intersection, group, set(LETTERS))
+    return functools.reduce(set.intersection, group)
 
 
 def part1():
-    groups = problem.GetBatches(
-        problem_number=6,
-        line_transform=set,
-        batch_transform=Part1Batch,
-    )
-    print(sum(len(g) for g in groups))
+    print(sum(len(g) for g in problem.GetBatches(6, set, Part1Batch)))
 
 
 def part2():
-    groups = problem.GetBatches(
-        problem_number=6,
-        line_transform=set,
-        batch_transform=Part2Batch,
-    )
-    print(sum(len(g) for g in groups))
+    print(sum(len(g) for g in problem.GetBatches(6, set, Part2Batch)))
 
 
 if __name__ == "__main__":
