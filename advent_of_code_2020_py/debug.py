4cd3ba41457113415fc25289554985a77499c65a
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2020-12-15 11:53:12 -0800

    try some fancy output formatting

---
+++ advent_of_code_2020_py/debug.py
@@ -0,0 +1,6 @@
+import rich
+from rich import traceback
+
+traceback.install()
+
+console = rich.console.Console()
--- advent_of_code_2020_py/problem15.py
+++ advent_of_code_2020_py/problem15.py
@@ -3,6 +3,8 @@
 import itertools
 from typing import Iterator, List, Sequence
 
+from advent_of_code_2020_py import debug
+
 
 def Parse(starting: str) -> List[int]:
     return [int(x) for x in starting.split(",")]
@@ -15,23 +17,28 @@ def Memory(starting: Sequence[int]) -> Iterator[int]:
     positions = {val: idx + 1 for idx, val in enumerate(starting)}
 
     for idx in itertools.count(len(positions)):
-        if last not in positions:
+        pos = positions.get(last)
+        if pos is None:
             result = 0
         else:
-            result = idx - positions[last]
+            result = idx - pos
         positions[last] = idx
         last = result
         yield result
 
 
 def part1():
+    debug.console.rule("[bold red]Part 1")
     starting = Parse("15,12,0,14,3,1")
-    print(list(itertools.islice(Memory(starting), 2019, 2020)))
+    debug.console.log(list(itertools.islice(Memory(starting), 2019, 2020)))
 
 
 def part2():
+    debug.console.rule("[bold red]Part 2")
     starting = Parse("15,12,0,14,3,1")
-    print(list(itertools.islice(Memory(starting), 30000000 - 1, 30000000)))
+    debug.console.log(
+        list(itertools.islice(Memory(starting), 30000000 - 1, 30000000))
+    )
 
 
 if __name__ == "__main__":
