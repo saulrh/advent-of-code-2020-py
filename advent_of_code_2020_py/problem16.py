19da83626b89871e7b570f51ecbddd255c2dd1d2
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2020-12-15 22:19:34 -0800

    solve day 16

---
+++ advent_of_code_2020_py/problem16.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+import copy
+import itertools
+import re
+from typing import Dict, Iterable, Iterator, List, Sequence, Tuple
+
+import attr
+
+from advent_of_code_2020_py import debug
+from advent_of_code_2020_py import problem
+
+
+@attr.s(auto_attribs=True, frozen=True)
+class Rule(object):
+    field: str
+
+    lower_lower: int
+    lower_upper: int
+    upper_lower: int
+    upper_upper: int
+
+    def __call__(self, field: int) -> bool:
+        return (self.lower_lower <= field <= self.lower_upper) or (
+            self.upper_lower <= field <= self.upper_upper
+        )
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __str__(self) -> str:
+        return (
+            f"<{self.field}: "
+            + f"{self.lower_lower}-{self.lower_upper}"
+            + " or "
+            + f"{self.upper_lower}-{self.upper_upper}>"
+        )
+
+
+@attr.s
+class ProblemStatement(object):
+    rules: List[Rule] = attr.ib(factory=list)
+    your_ticket: List[int] = attr.ib(factory=list)
+    tickets: List[List[int]] = attr.ib(factory=list)
+
+    def GetRule(self, field: str) -> Rule:
+        return next(r for r in self.rules if r.field == field)
+
+    def KnownInvalidTickets(self) -> Iterator[Tuple[List[int], List[int]]]:
+        for t in self.tickets:
+            if invalids := self.KnownInvalidFields(t):
+                yield t, invalids
+
+    def KnownInvalidFields(self, t: Iterable[int]) -> List[int]:
+        invalids = []
+        for x in t:
+            if all(not r(x) for r in self.rules):
+                invalids.append(x)
+        return invalids
+
+    def ValidTickets(self) -> Iterator[List[int]]:
+        for t in self.tickets:
+            if not self.KnownInvalidFields(t):
+                yield t
+
+    def DiscardInvalidTickets(self):
+        self.tickets = list(self.ValidTickets())
+
+
+_RULE_REGEX = re.compile(
+    r"(?P<field>.*): "
+    + r"(?P<lower_lower>\d+)"
+    + r"-"
+    + r"(?P<lower_upper>\d+)"
+    + r" or "
+    + r"(?P<upper_lower>\d+)"
+    + r"-"
+    + r"(?P<upper_upper>\d+)"
+)
+
+
+def ParseRule(line: str) -> Rule:
+    match = _RULE_REGEX.fullmatch(line)
+    if not match:
+        raise ValueError(f"Could not parse '{line}'")
+    return Rule(
+        field=match.group("field"),
+        lower_lower=int(match.group("lower_lower")),
+        lower_upper=int(match.group("lower_upper")),
+        upper_lower=int(match.group("upper_lower")),
+        upper_upper=int(match.group("upper_upper")),
+    )
+
+
+def Parse(ticket: Iterable[str]) -> ProblemStatement:
+    it = iter(ticket)
+    result = ProblemStatement()
+
+    def Next():
+        return next(it).strip()
+
+    line = Next()
+    while line:
+        result.rules.append(ParseRule(line))
+        line = Next()
+
+    line = Next()
+    assert line == "your ticket:"
+    line = Next()
+
+    result.your_ticket = [int(x) for x in line.split(",")]
+    line = Next()
+    line = Next()
+    assert line == "nearby tickets:"
+
+    for line in it:
+        line = line.strip()
+        result.tickets.append([int(x) for x in line.split(",")])
+
+    return result
+
+
+def PossibleColumns(
+    tickets: Sequence[Sequence[int]], rules: Iterable[Rule]
+) -> Dict[Rule, int]:
+    all_columns = set(range(len(tickets[0])))
+    columns = {r: copy.deepcopy(all_columns) for r in rules}
+    while any(len(cs) > 1 for cs in columns.values()):
+        for r, t, col in itertools.product(rules, tickets, all_columns):
+            if col in columns[r]:
+                if not r(t[col]):
+                    columns[r].discard(col)
+        for r in rules:
+            cols = columns[r]
+            if len(cols) == 1:
+                for other_r, other_cs in columns.items():
+                    if other_r != r:
+                        other_cs -= cols
+    result = {r: next(iter(cols)) for r, cols in columns.items()}
+    return result
+
+
+def part1():
+    debug.console.rule("[bold red]Part 1")
+    p = Parse(problem.Get(16))
+    debug.console.log(
+        sum(sum(invalids) for _, invalids in p.KnownInvalidTickets())
+    )
+
+
+def part2():
+    debug.console.rule("[bold red]Part 2")
+    p = Parse(problem.Get(16))
+    p.DiscardInvalidTickets()
+    columns = PossibleColumns(p.tickets, p.rules)
+    result = 1
+    for rule, column in columns.items():
+        if rule.field.startswith("departure "):
+            result *= p.your_ticket[column]
+    debug.console.log(result)
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
---
+++ tests/test_problem16.py
@@ -0,0 +1,79 @@
+import unittest
+
+from advent_of_code_2020_py import problem16
+
+DATA_1 = """class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12
+"""
+
+DATA_2 = """class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9
+"""
+
+
+class Test01(unittest.TestCase):
+    def test_Parse(self):
+        p = problem16.Parse(DATA_1.splitlines())
+        self.assertEqual(
+            p,
+            problem16.ProblemStatement(
+                your_ticket=[7, 1, 14],
+                tickets=[[7, 3, 47], [40, 4, 50], [55, 2, 20], [38, 6, 12]],
+                rules=[
+                    problem16.Rule("class", 1, 3, 5, 7),
+                    problem16.Rule("row", 6, 11, 33, 44),
+                    problem16.Rule("seat", 13, 40, 45, 50),
+                ],
+            ),
+        )
+
+    def test_Part1(self):
+        p = problem16.Parse(DATA_1.splitlines())
+        self.assertCountEqual(
+            list(p.KnownInvalidTickets()),
+            [
+                ([40, 4, 50], [4]),
+                ([55, 2, 20], [55]),
+                ([38, 6, 12], [12]),
+            ],
+        )
+        self.assertEqual(
+            sum(sum(invalids) for _, invalids in p.KnownInvalidTickets()), 71
+        )
+        self.assertEqual(list(p.ValidTickets()), [[7, 3, 47]])
+        p.DiscardInvalidTickets()
+        self.assertEqual(list(p.tickets), [[7, 3, 47]])
+
+    def test_PossibleColumns(self):
+        p = problem16.Parse(DATA_2.splitlines())
+        p.DiscardInvalidTickets()
+        columns = problem16.PossibleColumns(p.tickets, p.rules)
+        self.assertEqual(columns[p.GetRule("class")], 1)
+        self.assertEqual(columns[p.GetRule("row")], 0)
+        self.assertEqual(columns[p.GetRule("seat")], 2)
+        self.assertEqual(p.your_ticket[columns[p.GetRule("class")]], 12)
+        self.assertEqual(p.your_ticket[columns[p.GetRule("row")]], 11)
+        self.assertEqual(p.your_ticket[columns[p.GetRule("seat")]], 13)
+
+
+if __name__ == "__main__":
+    unittest.main()
