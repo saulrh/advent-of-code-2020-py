cb61f597e7ec3a46fa4c1d2bb7bc720ef040e971
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2020-12-19 01:10:23 -0800

    day 19 part 2
    aaaaaaaaaaaaaaaaaaah

--- advent_of_code_2020_py/problem19.py
+++ advent_of_code_2020_py/problem19.py
@@ -5,8 +5,7 @@ from __future__ import annotations
 from typing import List
 
 import attr
-import parsimonious
-from parsimonious import grammar
+import lark
 import regex
 
 from advent_of_code_2020_py import debug
@@ -15,28 +14,26 @@ from advent_of_code_2020_py import problem
 
 def RewriteToGrammar(inp: str) -> str:
     def RewriteLine(line):
-        line = line.replace(":", " = (").replace("|", ") / (") + " )"
-        return regex.sub(r"\d+", lambda m: "r" + m.group().zfill(6), line)
+        return regex.sub(r"\d+", lambda m: "r" + m.group(), line)
 
-    return "\n".join(sorted(RewriteLine(line) for line in inp.splitlines()))
+    return "\n".join(RewriteLine(line) for line in inp.splitlines())
 
 
 def RewriteForPart2(inp: str) -> str:
     inp = inp.replace("8: 42", "8: 42 | 42 8")
-    return inp.replace("11: 42 31", "11: 42 11? 31")
+    return inp.replace("11: 42 31", "11: 42 31 | 42 11 31")
 
 
 @attr.s(auto_attribs=True)
 class ProblemStatement(object):
-    g: grammar.Grammar
+    g: lark.Lark
     messages: List[str]
 
     @classmethod
     def FromStr(cls, inp: str) -> ProblemStatement:
         head, tail = inp.split("\n\n")
-        rewritten = RewriteToGrammar(head)
         return cls(
-            g=grammar.Grammar(rewritten),
+            g=lark.Lark(RewriteToGrammar(head), start="r0"),
             messages=[m.strip() for m in tail.splitlines()],
         )
 
@@ -44,7 +41,9 @@ class ProblemStatement(object):
         try:
             self.g.parse(m)
             return True
-        except parsimonious.exceptions.ParseError:
+        except lark.exceptions.UnexpectedCharacters:
+            return False
+        except lark.exceptions.UnexpectedEOF:
             return False
 
 
