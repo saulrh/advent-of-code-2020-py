4ffb633a7b9460311e4eca5d5b281c4c790c15aa
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2020-12-23 00:28:20 -0800

    solve day23 part 2

--- advent_of_code_2020_py/problem23.py
+++ advent_of_code_2020_py/problem23.py
@@ -2,40 +2,113 @@
 
 from __future__ import annotations
 
-from typing import List
+import itertools
+from typing import Dict, Iterable, Iterator
+
+import attr
 
 from advent_of_code_2020_py import debug
 
 
-def FindDestIdx(numbers: List[int]) -> int:
-    dest_num = numbers[0] - 1
-    while dest_num not in numbers[4:]:
+@attr.s(auto_attribs=True)
+class Buffer(object):
+    head: Node
+    nodes: Dict[int, Node]
+    largest: int
+
+    @classmethod
+    def Build(cls, numbers: Iterable[int]):
+        prv = None
+        largest = -1
+        head = None
+        nodes = {}
+        for n in numbers:
+            if head is None:
+                node = Node(car=n)
+                head = node
+            else:
+                node = Node(car=n, nxt=head, prv=prv)
+                prv.nxt = node
+            nodes[n] = node
+            largest = max(n, largest)
+            prv = node
+        prv.nxt = head
+        head.prv = prv
+        return cls(
+            head=head,
+            nodes=nodes,
+            largest=largest,
+        )
+
+    def __iter__(self) -> Iterator[Node]:
+        n = self.head
+        yield n
+        n = n.nxt
+        while n != self.head:
+            yield n
+            n = n.nxt
+
+    def Step(self):
+        dest = FindDest(self)
+        head = self.head
+        after_dest = dest.nxt
+        held1 = self.head.nxt
+        held2 = held1.nxt
+        held3 = held2.nxt
+        after_held = held3.nxt
+
+        # head -> held1 ... held3 -> after_held ... dest -> after_dest ... tail
+        head.SetNxt(after_held)
+        # head -> after_held ... dest -> after_dest ... tail
+        dest.SetNxt(held1)
+        held3.SetNxt(after_dest)
+        # head -> after_held ... dest -> held1 ... held3 -> after_dest ... tail
+        self.head = self.head.nxt
+        # after_head ... dest -> held1 ... held3 -> after_dest ... tail -> head
+
+
+@attr.s(auto_attribs=True, eq=False)
+class Node(object):
+    car: int
+    nxt: "Node" = attr.ib(factory=lambda self: self)
+    prv: "Node" = attr.ib(factory=lambda self: self)
+
+    def SetNxt(self, other):
+        self.nxt = other
+        other.prv = self
+
+    def SetPrv(self, other):
+        other.SetNxt(self)
+
+
+def FindDest(buf: Buffer) -> Node:
+    dest_num = buf.head.car
+    held1 = buf.head.nxt
+    held2 = held1.nxt
+    held3 = held2.nxt
+    held = [held1.car, held2.car, held3.car]
+    while True:
         dest_num -= 1
-        if dest_num < min(numbers):
-            dest_num = max(numbers)
-    return numbers.index(dest_num)
+        if dest_num < 1:
+            dest_num = buf.largest
+        if dest_num not in held:
+            return buf.nodes[dest_num]
 
 
-def Step(numbers: List[int]) -> List[int]:
-    dest_idx = FindDestIdx(numbers)
-    assert dest_idx >= 4
-    return (
-        numbers[4 : dest_idx + 1]
-        + numbers[1:4]
-        + numbers[dest_idx + 1 :]
-        + numbers[0:1]
-    )
+def Collect(buf: Buffer) -> str:
+    node = buf.nodes[1]
+    start = node
+    node = node.nxt
+    result = []
+    while node != start:
+        result.append(node.car)
+        node = node.nxt
+    return "".join(str(s) for s in result)
 
 
-def Collect(numbers: List[int]) -> str:
-    idx = numbers.index(1)
-    seq = numbers[idx + 1 :] + numbers[:idx]
-    return "".join(str(s) for s in seq)
-
-
-def Stars(numbers: List[int]) -> int:
-    idx = numbers.index(1)
-    return numbers[idx + 1] * numbers[idx + 2]
+def Stars(buf: Buffer) -> int:
+    node = buf.nodes[1]
+    return node.nxt.car * node.nxt.nxt.car
 
 
 INIT_STATE = [4, 1, 8, 9, 7, 6, 2, 3, 5]
@@ -43,14 +116,20 @@ INIT_STATE = [4, 1, 8, 9, 7, 6, 2, 3, 5]
 
 def part1():
     debug.console.rule("[bold red]Part 1")
-    state = INIT_STATE
+    state = Buffer.Build(INIT_STATE)
     for _ in range(100):
-        state = Step(state)
+        state.Step()
     debug.console.log(Collect(state))
 
 
 def part2():
     debug.console.rule("[bold red]Part 2")
+    state = Buffer.Build(
+        itertools.chain(INIT_STATE, range(max(INIT_STATE) + 1, 1000000 + 1))
+    )
+    for _ in range(10000000):
+        state.Step()
+    debug.console.log(Stars(state))
 
 
 if __name__ == "__main__":
--- tests/test_problem23.py
+++ tests/test_problem23.py
@@ -1,3 +1,4 @@
+import itertools
 import unittest
 
 from advent_of_code_2020_py import problem23
@@ -6,17 +7,37 @@ EXAMPLE_1 = [3, 8, 9, 1, 2, 5, 4, 6, 7]
 
 
 class Test20(unittest.TestCase):
+    def test_List(self):
+        state = problem23.Buffer.Build(range(10))
+        self.assertEqual(
+            list(s.car for s in state), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        )
+        state.head.nxt.nxt.nxt.SetNxt(state.head)
+        self.assertEqual(list(s.car for s in state), [0, 1, 2, 3])
+
     def test_Example1(self):
-        numbers = EXAMPLE_1
-        numbers = problem23.Step(numbers)
-        self.assertEqual(numbers, [2, 8, 9, 1, 5, 4, 6, 7, 3])
-        for _ in range(9):
-            numbers = problem23.Step(numbers)
-        self.assertEqual(numbers, [8, 3, 7, 4, 1, 9, 2, 6, 5])
-        self.assertEqual(problem23.Collect(numbers), "92658374")
+        state = problem23.Buffer.Build(EXAMPLE_1)
+        state.Step()
+        self.assertEqual(
+            list(n.car for n in state), [2, 8, 9, 1, 5, 4, 6, 7, 3]
+        )
+        state.Step()
+        self.assertEqual(
+            list(n.car for n in state), [5, 4, 6, 7, 8, 9, 1, 3, 2]
+        )
+        for _ in range(8):
+            state.Step()
+        self.assertEqual(
+            list(n.car for n in state), [8, 3, 7, 4, 1, 9, 2, 6, 5]
+        )
+        self.assertEqual(problem23.Collect(state), "92658374")
 
     def test_Example2(self):
-        numbers = EXAMPLE_1 + list(range(max(EXAMPLE_1) + 1, 1000000))
+        state = problem23.Buffer.Build(
+            itertools.chain(EXAMPLE_1, range(max(EXAMPLE_1) + 1, 1000000 + 1))
+        )
+        self.assertEqual(state.largest, 1000000)
+        self.assertEqual(len(state.nodes), 1000000)
         for _ in range(10000000):
-            numbers = problem23.Step(numbers)
-        self.assertEqual(problem23.Stars(numbers), 149245887792)
+            state.Step()
+        self.assertEqual(problem23.Stars(state), 149245887792)
